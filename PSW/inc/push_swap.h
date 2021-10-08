dc3331f36875352e433524a6b5ed19b4ae2d124f
iriska <thebluemitsu@gmail.com>
2021-10-08 04:27:16 +0100

    save

--- PSW/inc/push_swap.h
+++ PSW/inc/push_swap.h
@@ -2,9 +2,16 @@
 
 typedef	struct s_stack
 {
-	int* arr;
+	int *arr;
 	int size;
 }		t_stack;
+
+typedef	struct s_push_swap
+{
+	t_stack a;
+	t_stack b;
+
+}		t_stacks;
 
 void	display_stack(t_stack *stack);
 
--- PSW/src/markup.c
+++ PSW/src/markup.c
@@ -1,1 +1,41 @@
 #include "../inc/push_swap.h"
+
+void push_to_b(t_stacks stacks)
+{
+	int order;
+	int i;
+
+	i = 0;
+	order = find_order(stacks);
+	while (i < stacks->a.size - 1)
+	{
+		if(stacks->a.arr[0] == order)
+		{	
+			while(stacks->a.arr[0] < stacks->a.arr[1])
+			{	
+				pick("rra", stacks);
+				i++;
+			}
+			pick("rra", stacks);
+		}
+		else
+			pick("pb", stacks);
+		i++;
+	}
+}
+
+int index_a
+
+int find_best_index(t_stacks stacks)
+{
+	while (index_b < stacks->b.size)
+	{
+		if(index_b < stacks->b.size/2)
+			move = index_b + index_a(stacks, stacks->b.arr[index_b])
+		else
+			move = (size - index_b) + index_a(index_b)
+		if(best_move > move)
+			best_move = move;
+		index_b++;
+	}
+}
