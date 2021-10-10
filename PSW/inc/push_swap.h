bc9c75856a2c741f11dac8bd7ccc4bcf53410a06
iriska <thebluemitsu@gmail.com>
2021-10-10 19:09:55 +0100

    save

--- PSW/inc/push_swap.h
+++ PSW/inc/push_swap.h
@@ -13,32 +13,32 @@ typedef	struct s_push_swap
 
 }		t_stacks;
 
-void	display_stack(t_stack *stack);
-
 void	move_backward(t_stack *a, int from);
 void	move_forward(t_stack *a, int from);
-
-void	pb_until(int to, t_stack *a, t_stack *b);
-void	pa_all(t_stack *a, t_stack *b);
-int		is_in(int num, t_stack *stack);
-int		biggest(t_stack *stack);
-int		smallest(t_stack *stack);
+void	pick(char *cmd, t_stack *a, t_stack *b);
 
 void	push(t_stack *a, t_stack *b);
 void	swap(t_stack *a);
 void	reverse(t_stack *a);
 void	rotate(t_stack *a);
 
-void	pick(char *cmd, t_stack *a, t_stack *b);
-
 int		fillable(t_stack *stack, char **args);
 t_stack	*new_stack(int alloc, int size);
+void	move(char *smaller,char *bigger, int index, t_stacks *stacks)
 
-// int		sort(t_stack *a, t_stack *b);
-void smallsort(t_stack *a);
-void fivesort(t_stack *a, t_stack *b);
+void	push_to_b(t_stacks stacks);
+void	push_to_a(t_stacks *stacks):
+
+// not needed - delete
+void	pb_until(int to, t_stack *a, t_stack *b);
+void	pa_all(t_stack *a, t_stack *b);
+int		is_in(int num, t_stack *stack);
+int		biggest(t_stack *stack);
+int		smallest(t_stack *stack);
+void 	smallsort(t_stack *a);
+void 	fivesort(t_stack *a, t_stack *b);
+void	bubble_sort(t_stack *a, t_stack *b);
 
 /*** ^ T E S T ^ ***/
 void	display_stack(t_stack *a);
-void	bubble_sort(t_stack *a, t_stack *b);
 int		is_sorted(t_stack *stack);
