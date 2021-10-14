66629b92aab6ad362b6d79aee2c9e871687125be
iriska <thebluemitsu@gmail.com>
2021-10-14 02:50:11 +0100

    save

--- PSW/src/push_swap_utils.c
+++ PSW/src/push_swap_utils.c
@@ -3,10 +3,10 @@
 int index_a(t_stacks *stacks, int num);
 int movement(t_stack stack, int index);
 int best_index(t_stacks *stacks);
-int	find_order(t_stacks *stacks);
+int seperate_order(t_stacks *stacks);
 
 //push everything to b except for the ordered part
-void push_to_b(t_stacks *stacks, int flag) //
+void push_to_b(t_stacks *stacks, int flag) 
 {
 	int order;
 	int i;
@@ -143,33 +143,4 @@ int best_index(t_stacks *stacks)
 		index_b++;
 	}
 	return (best_index);
-}
-
-int	find_order(t_stacks *stacks)
-{
-	int	i;
-	int order;
-	int biggest_order;
-	int	order_number;
-
-	i = 0;
-	biggest_order = 0;
-	order_number= stacks->a.arr[0];
-	order = 0;
-	while (i < stacks->a.size - 1)
-	{
-		if (stacks->a.arr[i] < stacks->a.arr[i + 1])
-			order++;
-		else
-		{	
-			if (biggest_order < order)
-			{	
-				biggest_order = order;
-				order_number = stacks->a.arr[i - order];
-			}
-			order = 0;
-		}
-		i++;
-	}
-	return (order_number);
 }
