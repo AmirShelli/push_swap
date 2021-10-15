6bfab5359c9abf1f6e0000d665392794431af4c2
iriska <thebluemitsu@gmail.com>
2021-10-15 14:50:51 +0300

    save

--- PSW_BUT_KITTYS!/inc/push_swap.h
+++ PSW_BUT_KITTYS!/inc/push_swap.h
@@ -40,7 +40,8 @@ int			get_min_max(t_stack *stack, int min_max);
 t_stack		*new_stack(int num);
 void		stack_push_front(t_stack **stack, t_stack *new_element);
 t_stack		*stack_last_element(t_stack *stack);
-t_stack		*get_value(t_stack *stack, int i);
+t_stack		*get_value(t_stack *stack, int index);
+int			get_index(t_stack *stack, int value);
 void		free_stack(t_stack *stack);
 
 void		pick(char *cmd, t_push_swap *stacks);
