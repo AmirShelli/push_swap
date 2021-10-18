37b029373db8aef148a5884012333069facf3f6f
iriska <thebluemitsu@gmail.com>
2021-10-18 18:30:37 +0300

    save

---
+++ TEST/test.c
@@ -0,0 +1,20 @@
+#include "../../inc/push_swap.h"
+#include <stdio.h>
+
+void	print_stack(t_stack *stack, char st) 
+{
+	int index = 0;
+	printf("%c: ", st);
+	if (!stack)
+	{
+		printf("\n");
+		return ;
+	}
+	while (stack->next)
+	{
+		printf("[%d]{ %d }| ", index, stack->num);
+		stack = stack->next;
+		index++;
+	}
+	printf("[%d]{ %d }|\n", index, stack->num);
+}
